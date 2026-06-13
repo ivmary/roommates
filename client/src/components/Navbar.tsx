53e65dc18e1c5de2a9983434434d35fa5e5482d6
ivmary <mary1.ivashenko@gmail.com>
2026-06-13 22:31:01 +0300

    linting and changing name

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import "./Navbar.css";
 
 export default function Navbar() {
   const { user, logout } = useAuth();
@@ -9,18 +9,24 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-inner">
         <Link to="/" className="navbar-logo">
-          🏠 PartDorms
+          🏠 RoomMates
         </Link>
         <div className="navbar-actions">
           {user ? (
             <>
-              <span className="navbar-user">Hi, {user.name.split(' ')[0]}</span>
-              <button className="btn btn-ghost" onClick={logout}>Log out</button>
+              <span className="navbar-user">Hi, {user.name.split(" ")[0]}</span>
+              <button className="btn btn-ghost" onClick={logout}>
+                Log out
+              </button>
             </>
           ) : (
             <>
-              <Link to="/login" className="btn btn-ghost">Log in</Link>
-              <Link to="/register" className="btn btn-primary">Sign up</Link>
+              <Link to="/login" className="btn btn-ghost">
+                Log in
+              </Link>
+              <Link to="/register" className="btn btn-primary">
+                Sign up
+              </Link>
             </>
           )}
         </div>
