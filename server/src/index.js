587e7cef4ac2fd0dddd6f9ed8f831a760ab99b68
ivmary <mary1.ivashenko@gmail.com>
2026-07-02 22:08:03 +0300

    create and register uploads

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
+
+const uploadsDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(uploadsDir));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/apartments', require('./routes/apartments'));
