7ca2f12c9fcb1e3f88c3837076e2ba53d9187084
ivmary <mary1.ivashenko@gmail.com>
2026-07-12 00:50:21 +0300

    creating a new conversation is only possible after the first message is sent via the server side, so no http route is needed

--- server/src/routes/conversations.js
+++ server/src/routes/conversations.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {
-  createConversation,
-  getConversations,
-  getMessages,
-} = require('../controllers/conversationController');
+const { getConversations, getMessages } = require('../controllers/conversationController');
 const { protect } = require('../middleware/auth');
 
-router.post('/', protect, createConversation);
 router.get('/', protect, getConversations);
 router.get('/:id/messages', protect, getMessages);
 
